0481699d13a0c28119cc41d0bad74128e435b365
LINH <hohoanglinh190295@gmail.com>
2017-12-14 19:31:33 +0700

    HANIE KEC

--- app/src/main/java/com/sega/lolproject/activity/MainActivity.kt
+++ app/src/main/java/com/sega/lolproject/activity/MainActivity.kt
@@ -8,10 +8,14 @@ import android.os.Bundle
 import android.support.annotation.StyleRes
 import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Priority
+import com.bumptech.glide.request.RequestOptions
 import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
@@ -31,7 +35,14 @@ import kotlinx.android.synthetic.main.activity_main.*
  */
 
 
-class MainActivity :YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
+class MainActivity :YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener{
+//    override fun onNothingSelected(parent: AdapterView<*>?) {
+//    }
+//
+//    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+//    }
+
     override fun onInitializationSuccess(p0: YouTubePlayer.Provider?, p1: YouTubePlayer?, p2: Boolean) {
         if (null == p1) return
         youTubePlayer = p1
@@ -68,7 +79,7 @@ class MainActivity :YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
     private val pics = intArrayOf(R.drawable.p1, R.drawable.p2, R.drawable.p3, R.drawable.p4, R.drawable.p5)
     private val maps = intArrayOf(R.drawable.map_paris, R.drawable.map_seoul, R.drawable.map_london, R.drawable.map_beijing, R.drawable.map_greece)
     private val descriptions = intArrayOf(R.string.text1, R.string.text2, R.string.text3, R.string.text4, R.string.text5)
-
+    val listchampnomana = arrayOf("Aatrox","DrMundo","Vladimir","Mordekaiser","Garen","Katarina","Riven","Tryndamere","Renekton","Shyvana","Rengar","Rumble")
     private var layoutManger: CardSliderLayoutManager? = null
     private var recyclerView: RecyclerView? = null
     private var mapSwitcher: ImageSwitcher? = null
@@ -87,15 +98,40 @@ class MainActivity :YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
 
     private var decodeMapBitmapTask: DecodeBitmapTask? = null
     private var mapLoadListener: DecodeBitmapTask.Listener? = null
-
+    val options = RequestOptions()
+            .centerCrop()
+            .dontAnimate()
+            .placeholder(R.drawable.img_error)
+            .error(R.drawable.img_error)
+            .priority(Priority.HIGH)!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-       champion =  RealmController.with(this).getChampion("Zoe")
+       champion =  RealmController.with(this).getChampion("Azir")
         println(champion!!.name)
         sliderAdapter = SliderAdapter(champion!!.skins!!, champion!!.skins!!.size, OnCardClickListener())
+        var adapter_level = ArrayAdapter<String> (this,R.layout.spinner_item, resources.getStringArray(R.array.level))
+        adapter_level.setDropDownViewResource(android.R.layout.simple_spinner_item)
+        spinner_level.adapter = adapter_level
+        spinner_level.setSelection(0)
+        spinner_level.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
+            override fun onItemSelected(parent: AdapterView<*>, view: View, pos: Int, id: Long) {
+            var a = resources.getStringArray(R.array.level)
+//                Log.e("SELECT",a[pos])
+                when(pos) {
+                    0 -> getFullStat118(champion!!)
+                    else -> getFullStatLevel(a[pos],champion!!)
+                }
+
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<out Adapter>?) {
+
+            }
+
+        }
         initRecyclerView()
 
         initSwitchers(champion!!)
@@ -127,8 +163,47 @@ class MainActivity :YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
             decodeMapBitmapTask!!.cancel(true)
         }
     }
+    fun getFullStat118(champion : Champion) {
+        health.text = champion.stats?.hp + " - " + getStat("18", champion.stats?.hp!!, champion.stats?.hpperlevel!!)
+        healthregen.text = champion.stats?.hpregen + " - " + getStat("18", champion.stats?.hpregen!!, champion.stats?.hpregenperlevel!!)
+        if (listchampnomana.contains(champion.id)) {
+            mana.text = ""
+            manaregen.text = ""
+        } else {
+            mana.text = champion.stats?.mp + " - " + getStat("18", champion.stats?.mp!!, champion.stats?.mpperlevel!!)
+            manaregen.text = champion.stats?.mpregen + " - " + getStat("18", champion.stats?.mpregen!!, champion.stats?.mpregenperlevel!!)
 
+        }
+        range.text = champion.stats?.attackrange
+        attackdmg.text = champion.stats?.attackdamage + " - " + getStat("18", champion.stats?.attackdamage!!, champion.stats?.attackdamageperlevel!!)
+        attackspeed.text = getAttackSpeed(champion.stats?.attackspeedoffset!!).toString()
+        armor.text = champion.stats?.armor + " - " + getStat("18", champion.stats?.armor!!, champion.stats?.armorperlevel!!)
+        magicresist.text = champion.stats?.spellblock + " - " + getStat("18", champion.stats?.spellblock!!, champion.stats?.spellblockperlevel!!)
+        movespeed.text = champion.stats?.movespeed
+    }
+        fun getFullStatLevel(level : String , champion : Champion){
+            health.text = getStat(level, champion.stats?.hp!!,champion.stats?.hpperlevel!!).toString()
+            healthregen.text = getStat(level, champion.stats?.hpregen!!,champion.stats?.hpregenperlevel!!).toString()
+            if(listchampnomana.contains(champion.id)){
+                mana.text = ""
+                manaregen.text = ""
+            } else {
+                mana.text = getStat(level, champion.stats?.mp!!,champion.stats?.mpperlevel!!).toString()
+                manaregen.text = getStat(level, champion.stats?.mpregen!!,champion.stats?.mpregenperlevel!!).toString()
+
+            }
+            range.text = champion.stats?.attackrange
+            attackdmg.text = getStat(level, champion.stats?.attackdamage!!,champion.stats?.attackdamageperlevel!!).toString()
+            attackspeed.text =  getAttackSpeed(champion.stats?.attackspeedoffset!!).toString()
+            armor.text = getStat(level, champion.stats?.armor!!,champion.stats?.armorperlevel!!).toString()
+            magicresist.text = getStat(level, champion.stats?.spellblock!!,champion.stats?.spellblockperlevel!!).toString()
+            movespeed.text = champion.stats?.movespeed
+        }
     private fun initSwitchers(champion : Champion) {
+
+
+        Log.e("stat",getStat("8","68","3.2").toString())
+//        price.text = champion?.price
         temperatureSwitcher = findViewById<View>(R.id.ts_temperature) as TextSwitcher
         temperatureSwitcher!!.setFactory(TextViewFactory(R.style.TemperatureTextView, true))
         temperatureSwitcher!!.setCurrentText("220 RP")
@@ -151,6 +226,11 @@ class MainActivity :YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
         youTubePlayerView?.initialize(API_KEY, this)
 
         linkSkin = champion.skins!![0]!!.link
+        Glide.with(this)
+                .load(champion!!.imageAvatar)
+                .thumbnail(0.1f)
+                .apply(options)
+                .into(avattarChamp)
         onActiveCardChange(0)
     }
 
@@ -273,7 +353,14 @@ class MainActivity :YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
             return imageView
         }
     }
-
+    fun  getStat(level : String, base : String, growth : String ): Double {
+        var stat = base.toDouble() + growth.toDouble() * (level.toInt() - 1) * (0.685 + 0.0175 * level.toInt())
+        return Math.round(stat*10)/10.toDouble()
+    }
+    fun getAttackSpeed(attackdelay : String): Double {
+        var speed = 0.625/(1+attackdelay.toDouble())
+        return Math.round(speed*1000)/1000.toDouble()
+    }
     private inner class OnCardClickListener : View.OnClickListener {
         override fun onClick(view: View) {
             val lm = recyclerView!!.layoutManager as CardSliderLayoutManager
